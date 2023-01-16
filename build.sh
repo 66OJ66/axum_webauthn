a141d2670f8426cc7aeed65435abfecf3d63af13
66OJ66 <hi0obxud@anonaddy.me>
2023-01-16 22:22:56 +0000

    Pre-compress assets during the build process

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "Starting build"
 cargo build --release
+echo "Clearing existing assets"
+rm -r target/release/assets
 echo "Copying assets to target"
 cp -r assets target/release
+echo "Compressing assets with gzip"
+gzip -r -k -9 target/release/assets/
 echo "Finished build"
